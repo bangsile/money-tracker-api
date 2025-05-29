a59372033c3a6431f7b37cd82dfb4c13dddc9bc0
bangsile <asilt94@gmail.com>
2025-05-29 22:48:35 +0900

    Update logging format
    Update logging format for better readable

--- src/application/logging.ts
+++ src/application/logging.ts
@@ -1,9 +1,17 @@
 import * as winston from "winston";
 
 export const logger = winston.createLogger({
-    level: "debug",
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.Console({})
-    ]
-})
+	level: "debug",
+	format: winston.format.combine(
+		winston.format.colorize(),
+		winston.format.timestamp(),
+		winston.format.printf(({ level, message, timestamp }) => {
+			const msg =
+				typeof message === "object"
+					? JSON.stringify(message, null, 2)
+					: message;
+			return `[${timestamp}] ${level}: ${msg}`;
+		})
+	),
+	transports: [new winston.transports.Console({})],
+});
